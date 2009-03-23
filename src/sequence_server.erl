564ba979dd25b1b93e6e02533c0c6ce541a7077b
averyj <javery@infozerk.com>
2009-03-23 12:38:42 -0400

    formating changes

--- src/sequence_server.erl
+++ src/sequence_server.erl
@@ -2,9 +2,9 @@
 %%% File:      sequence_server.erl
 %%% @author    James Avery
 %%% @copyright 2009 James Avery
-%%% @doc  
+%%% @doc
 %%%
-%%% @end  
+%%% @end
 %%%
 %%% @since 2009-03-18 by James Avery
 %%%-------------------------------------------------------------------
@@ -13,7 +13,7 @@
 
 -behaviour(gen_server).
 -export([start_link/0, get_sequence/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, 
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
         terminate/2, code_change/3]).
 
 -record(sequence, {id, sequence}).
@@ -23,33 +23,33 @@ start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 get_sequence(Id) ->
-	mnesia:wait_for_tables([sequence], infinity),
-	gen_server:call(?MODULE, {get_sequence, Id}).
+  mnesia:wait_for_tables([sequence], infinity),
+  gen_server:call(?MODULE, {get_sequence, Id}).
 
 init([]) ->
-	mnesia:start(),
-	mnesia:create_schema([node()]),	
-	mnesia:create_table(sequence, [{attributes, record_info(fields, sequence)},
-                                 	{type, ordered_set},
-                                    {disc_copies, [node()]}]),
+  mnesia:start(),
+  mnesia:create_schema([node()]),
+  mnesia:create_table(sequence, [{attributes, record_info(fields, sequence)},
+                                 {type, ordered_set},
+                                 {disc_copies, [node()]}]),
   {ok, #state{}}.
 
 handle_call({get_sequence, Id}, _From, State) ->
   F = fun() ->
-	  Return = mnesia:read(sequence, Id, write),
-	  case Return of
-	    [S] -> 	
-	   	  Seq = S#sequence.sequence,
-	   	  New = S#sequence{sequence = Seq + 1},
-	      mnesia:write(New),
-		  Seq;
-	    [] ->
-		  SequenceRecord = #sequence{id = Id, sequence = 2},
-		  mnesia:write(SequenceRecord),
-		  1
-		  end
-   	end,
-	{atomic, Seq} = mnesia:transaction(F),
+    Return = mnesia:read(sequence, Id, write),
+    case Return of
+      [S] ->
+        Seq = S#sequence.sequence,
+        New = S#sequence{sequence = Seq + 1},
+        mnesia:write(New),
+        Seq;
+      [] ->
+        SequenceRecord = #sequence{id = Id, sequence = 2},
+        mnesia:write(SequenceRecord),
+        1
+    end
+  end,
+  {atomic, Seq} = mnesia:transaction(F),
   {reply, Seq, State};
 
 handle_call(_Request, _From, State) ->
